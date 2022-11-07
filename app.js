4412dd25f476f03ac9216ff5aebd68c9318e8997
AlexisFLT <alexisfoulont@gmail.com>
2022-11-07 12:19:11 +0100

    solution express03

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = 5000;
 
 const welcome = (req, res) => {
@@ -17,9 +19,10 @@ const userHandlers = require("./userHandlers");
 
 // app.get("/api/movies", movieHandlers.getMovies);
 // app.get("/api/movies/:id", movieHandlers.getMovieById);
-
+// app.post("/api/movies", movieHandlers.postMovie);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUsersById);
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -1,5 +1,3 @@
-const database = require("./database");
-
 const user = [
   {
     id: 1,
@@ -50,6 +48,24 @@ const user = [
     language: "Spanish",
   },
 ];
+const database = require("./database");
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users (firstname, lastname, email, city, language ) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
 
 const getUsers = (req, res) => {
   database
@@ -84,4 +100,5 @@ const getUsersById = (req, res) => {
 module.exports = {
   getUsers,
   getUsersById,
+  postUser,
 };
